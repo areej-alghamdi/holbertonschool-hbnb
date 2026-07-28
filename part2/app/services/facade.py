3141cb6798f7526ff1abe3f3f8c5b8086a96c5ba
ahaad14550-boop <ahaad.14550@gmail.com>
2026-07-28 14:05:03 +0300

    add line import review

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -2,6 +2,7 @@ from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 from app.models.amenity import Amenity
 from app.models.place import Place
+from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
