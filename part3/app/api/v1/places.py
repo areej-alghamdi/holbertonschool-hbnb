c7d63fb2e733c54ea12ebf97adb0edcbb0b02908
Hadeel <hadeel.alqhtani206@gmail.com>
2026-08-05 05:08:01 +0300

    Update places.py

--- part3/app/api/v1/places.py
+++ part3/app/api/v1/places.py
@@ -1,7 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 from app.services import facade
-from flask import request
 
 api = Namespace('places', description='Place operations')
 
