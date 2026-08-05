eb36145792e1962a71cbb5acfa0aeee462ce69dc
Hadeel <hadeel.alqhtani206@gmail.com>
2026-08-05 05:08:17 +0300

    Update reviews.py

--- part3/app/api/v1/reviews.py
+++ part3/app/api/v1/reviews.py
@@ -1,7 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
 from app.services import facade
-from flask import request
 
 api = Namespace('reviews', description='Review operations')
 
